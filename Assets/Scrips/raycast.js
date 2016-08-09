1fea2813ff213a0e1dbfc0c64478caa0e1e5da5e
juaniman99 <juaniman.2000@gmail.com>
2016-08-09 23:52:55 +0200

    Prueba de juanimi

--- Assets/Scrips/raycast.js
+++ Assets/Scrips/raycast.js
@@ -1,5 +1,7 @@
 ﻿#pragma strict
 
+//Hola soy Juan
+
 function Start () {
 
 }
@@ -7,7 +9,7 @@ function Start () {
 function Update () {
 	var hit : RaycastHit;
 	if(Physics.Raycast(this.transform.position, Vector3(0,-1,0), hit, 1)){
-	Debug.Log(hit.collider.gameObject);
+
 	}
 	
 }
