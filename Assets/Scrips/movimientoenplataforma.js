c47c6649d807b350c91ec76e455a003d66091987
juaniman99 <juaniman.2000@gmail.com>
2016-08-09 23:52:03 +0200

    Prueba de juanimi

---
+++ Assets/Scrips/movimientoenplataforma.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(coll : Collider){
+	if(coll.tag == "moviente"){
+	this.transform.parent = coll.gameObject.transform;
+	}
+}
+
+function OnCollisionExit(coll : Collider){
+	if(coll.tag == "moviente"){
+	this.transform.parent = null;
+	}
+}
