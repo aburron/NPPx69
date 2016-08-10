4227a86a5e2355e207bee3ada11b0f48e4055bdf
aburron <aburron@hotmail.com>
2016-08-10 02:23:58 +0200

    Ciclo día-noche

---
+++ Assets/Scrips/dayNightCycle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dayNightCycle : MonoBehaviour {
+
+	public float speed;
+
+	void Update () 
+	{
+		transform.Rotate (Vector3.right * speed * Time.deltaTime);
+	}
+}
