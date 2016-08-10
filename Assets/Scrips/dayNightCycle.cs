784fdffadcdd06d0e26d723d26deee3466d6abec
aburron <aburron@hotmail.com>
2016-08-10 13:16:51 +0200

    Revert "Prueba de hoguera y skybox"
    This reverts commit e8ce3804684b7a24882bd80c0f337c9df745c414.

--- Assets/Scrips/dayNightCycle.cs
+++ Assets/Scrips/dayNightCycle.cs
@@ -4,37 +4,9 @@ using System.Collections;
 public class dayNightCycle : MonoBehaviour {
 
 	public float speed;
-	public Material SkyboxDia; //Cielo de dia
-	public Material SkyboxNoche; //Cielo noche
 
 	void Update () 
 	{//La luz direccional va girando, creando un ciclo de dia y noche
 		transform.Rotate (Vector3.right * speed * Time.deltaTime);
-	}
-	//A partir de aqui es para cambiar el cielo a uno estrellado.
-
-	void Start ()
-	{
-		StartCoroutine(diawait());
-	}
-	public void dia()
-	{
-
-		RenderSettings.skybox = SkyboxNoche;
-		//noche ();
-	}
-	IEnumerator diawait()
-	{
-		print("dia");
-		yield return new WaitForSeconds(5);
-		RenderSettings.skybox = SkyboxNoche;
-		StartCoroutine(nochewait());
-	}
-	IEnumerator nochewait()
-	{
-		print("noche");
-		yield return new WaitForSeconds(5);
-		RenderSettings.skybox = SkyboxDia;
-		StartCoroutine(diawait());
 	}
 }
