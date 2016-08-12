de65ec42e4e212597fe07cc284ec0d22aab88944
aburron <aburron@hotmail.com>
2016-08-12 16:30:07 +0200

    aaaaaaa

---
+++ Assets/Scrips/Dialogue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour 
+{
+	public GameObject targetPlayer;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (Input.GetKey (KeyCode.E))
+		{
+			if (coll.tag == "Player")
+			{
+				targetPlayer.SetActive (true);
+			}
+		}
+	}
+}
